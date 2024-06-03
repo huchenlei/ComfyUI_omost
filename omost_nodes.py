a36d75660d89c94f081023752a1588dd5ff38cd2
huchenlei <chenlei.hu@mail.utoronto.ca>
2024-06-03 11:20:02 -0400

    Add llm inference logic

--- omost_nodes.py
+++ omost_nodes.py
@@ -1,8 +1,8 @@
 from __future__ import annotations
-from typing import Literal, Tuple, TypedDict
+from typing import Literal, Tuple, TypedDict, NamedTuple
 
 import torch
-from transformers import AutoModelForCausalLM
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
 import comfy.model_management
 from .lib_omost.canvas import Canvas as OmostCanvas
@@ -15,6 +15,12 @@ class OmostConversationItem(TypedDict):
 
 
 OmostConversation = list[OmostConversationItem]
+
+
+class OmostLLM(NamedTuple):
+    model: AutoModelForCausalLM
+    tokenizer: AutoTokenizer
+
 
 # End of type definitions.
 
@@ -37,7 +43,7 @@ class OmostLLMLoaderNode:
     RETURN_TYPES = ("OMOST_LLM",)
     FUNCTION = "load_llm"
 
-    def load_llm(self, llm_name: str) -> Tuple[AutoModelForCausalLM]:
+    def load_llm(self, llm_name: str) -> Tuple[OmostLLM]:
         """Load LLM model"""
         HF_TOKEN = None
         dtype = (
@@ -50,8 +56,9 @@ class OmostLLMLoaderNode:
             token=HF_TOKEN,
             device_map="auto",  # This will load model to gpu with an offload system
         )
+        llm_tokenizer = AutoTokenizer.from_pretrained(llm_name, token=HF_TOKEN)
 
-        return (llm_model,)
+        return (OmostLLM(llm_model, llm_tokenizer),)
 
 
 class OmostLLMChatNode:
@@ -61,6 +68,18 @@ class OmostLLMChatNode:
             "required": {
                 "llm": ("OMOST_LLM",),
                 "text": ("STRING", {"multiline": True}),
+                "max_new_tokens": (
+                    "INT",
+                    {"min": 128, "max": 4096, "step": 1, "default": 4096},
+                ),
+                "top_p": (
+                    "FLOAT",
+                    {"min": 0.0, "max": 1.0, "step": 0.01, "default": 0.9},
+                ),
+                "temperature": (
+                    "FLOAT",
+                    {"min": 0.0, "max": 2.0, "step": 0.01, "default": 0.6},
+                ),
             },
             "optional": {
                 "conversation": ("OMOST_CONVERSATION",),
@@ -75,13 +94,44 @@ class OmostLLMChatNode:
 
     def run_llm(
         self,
-        llm: AutoModelForCausalLM,
+        llm: OmostLLM,
         text: str,
-        conversation: OmostConversation,
+        max_new_tokens: int,
+        top_p: float,
+        temperature: float,
+        conversation: OmostConversation | None = None,
     ) -> Tuple[OmostConversation, OmostCanvas]:
         """Run LLM on text"""
+        llm_tokenizer: AutoTokenizer = llm.tokenizer
+        llm_model: AutoModelForCausalLM = llm.model
 
+        conversation = conversation or []  # Default to empty list
         new_conversation = conversation + [{"role": "user", "content": text}]
+
+        input_ids: torch.Tensor = llm_tokenizer.apply_chat_template(
+            new_conversation, return_tensors="pt", add_generation_prompt=True
+        ).to(llm_model.device)
+
+        output_ids = llm_model.generate(
+            input_ids=input_ids,
+            max_new_tokens=max_new_tokens,
+            temperature=temperature,
+            top_p=top_p,
+            do_sample=temperature != 0,
+        )
+
+        generated_text: str = llm_tokenizer.decode(
+            output_ids[0],
+            skip_special_tokens=True,
+            skip_prompt=True,
+            timeout=10,
+        )
+
+        final_conversation = new_conversation + [
+            {"role": "assistant", "content": generated_text}
+        ]
+
+        return (final_conversation, OmostCanvas.from_bot_response(generated_text))
 
 
 class OmostLayoutCondNode:
